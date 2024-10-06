e892f5ecc33a92fbc4c5f017987e47f84544e3dd
染川 瞳 <me@hareru.org>
2024-10-06 21:45:04 +0900

    Daily Update

--- layouts/imgbox.php
+++ layouts/imgbox.php
@@ -3,9 +3,7 @@ include(get_stylesheet_directory().'/layouts/all_opt.php');
 
 $text_logo = iro_opt('text_logo');
 
-$print_social_zone = function() use ($all_opt, $social_display_icon): void {
-
-    // 微信
+function print_social_zone($all_opt, $social_display_icon) {
     if (iro_opt('wechat')): ?>
         <li class="wechat"><a href="#" title="WeChat"><img loading="lazy" src="<?= $social_display_icon ?>wechat.png" /></a>
             <div class="wechatInner">
@@ -14,7 +12,6 @@ $print_social_zone = function() use ($all_opt, $social_display_icon): void {
         </li>
     <?php endif;
 
-    // 大体(all_opt.php)
     foreach ($all_opt as $key => $value):
         if (!empty($value['link'])):
             $img_url = $value['img'] ?? ($social_display_icon . ($value['icon'] ?? $key) . '.png');
@@ -23,14 +20,11 @@ $print_social_zone = function() use ($all_opt, $social_display_icon): void {
         <?php endif;
     endforeach;
 
-    // 邮箱
     if (iro_opt('email_name') && iro_opt('email_domain')): ?>
         <li><a onclick="mail_me()" class="social-wangyiyun" title="E-mail"><img loading="lazy" alt="E-mail" src="<?= iro_opt('vision_resource_basepath') ?><?= iro_opt('social_display_icon') ?>/mail.png" /></a></li>
     <?php endif;
+}
 
-};
-
-// 获取作者信息
 $args = array(
     'role__in' => array('Administrator', 'Editor', 'Author', 'Contributor'),
     'has_published_posts' => true,
@@ -45,27 +39,20 @@ $author_info = [];
 if ($author_count == 1) {
     $author = $authors[0];
     $post_count = count_user_posts($author->ID, 'post');
-
-    // 获取该作者所有文章的ID
     $author_posts = get_posts(array(
         'author' => $author->ID,
         'post_type' => 'post',
         'posts_per_page' => -1,
         'fields' => 'ids'
     ));
-
-    // 统计这些文章的评论数量
     $comment_count = get_comments(array(
         'post__in' => $author_posts,
         'count' => true
     ));
-
-    // 获取作者介绍并限制字数
     $author_description = get_the_author_meta('description', $author->ID);
     if (mb_strlen($author_description, 'UTF-8') > 25) {
         $author_description = mb_substr($author_description, 0, 25, 'UTF-8') . '...';
     }
-
     $author_info = [
         'type' => 'single',
         'author' => $author,
@@ -76,7 +63,16 @@ if ($author_count == 1) {
 } elseif ($author_count == 2) {
     $author_info = [
         'type' => 'double',
-        'authors' => $authors
+        'authors' => array_map(function($author) {
+            $author_description = get_the_author_meta('description', $author->ID);
+            if (mb_strlen($author_description, 'UTF-8') > 25) {
+                $author_description = mb_substr($author_description, 0, 25, 'UTF-8') . '...';
+            }
+            return [
+                'author' => $author,
+                'author_description' => $author_description
+            ];
+        }, $authors)
     ];
 } elseif ($author_count >= 3) {
     $author_info = [
@@ -97,7 +93,6 @@ if ($author_count == 1) {
             <?php endif; ?>
             <div class="header-container">
                 <div class="header-info">
-                    <!-- 首页一言打字效果 -->
                     <?php if (iro_opt('signature_typing', 'true')): ?>
                         <?php if (iro_opt('signature_typing_marks', 'true')): ?><i class="fa-solid fa-quote-left"></i><?php endif; ?>
                         <span class="element"><?= iro_opt('signature_typing_placeholder', '疯狂造句中......') ?></span>
@@ -143,8 +138,8 @@ if ($author_count == 1) {
                 }
                 ?>
             </div>
-                <?php if ($author_info['type'] == 'single'): ?>
-                    <div class="hwcard-content-unlimited">
+            <?php if ($author_info['type'] == 'single'): ?>
+                <div class="hwcard-content-unlimited">
                     <div class="hwcard-author">
                         <div class="hwcard-author-avatar-s">
                             <svg width="120" height="120" viewBox="0 0 120 120" class="author-avatar-svg">
@@ -173,45 +168,52 @@ if ($author_count == 1) {
                         </div>
                     </div>
                 </div>
-                <?php elseif ($author_info['type'] == 'double'): ?>
-                    <div class="hwcard-content">
-    <?php foreach ($author_info['authors'] as $author): ?>
-        <div class="hwcard-author-d">
-            <div class="hwcard-author-avatar-d">
-                <a href="<?= get_author_posts_url($author->ID); ?>">
-                    <?= get_avatar($author->ID, 48); ?>
-                </a>
-            </div>
-            <a href="<?= get_author_posts_url($author->ID); ?>">
-                    <span class="hwcard-author-name-d"><?= esc_html($author->display_name); ?></span>
-                </a>
-            <div class="hwcard-author-data-item-d">
-                <span class="hwcard-author-data-value-d"><?= count_user_posts($author->ID); ?></span>
-                <span class="hwcard-author-data-label-d"><?= esc_html__('Posts', 'sakurairo'); ?></span>
-            </div>
-        </div>
-    <?php endforeach; ?>
-</div>
-                <?php elseif ($author_info['type'] == 'multiple'): ?>
-                    <div class="hwcard-content">
-                    <?php foreach ($author_info['authors'] as $author): ?>
-                            <div class="hwcard-author-avatar">
-                                <a href="<?= get_author_posts_url($author->ID); ?>">
-                                    <?= get_avatar($author->ID, 48); ?>
+            <?php elseif ($author_info['type'] == 'double'): ?>
+                <div class="hwcard-content">
+                    <?php foreach ($author_info['authors'] as $author_data): ?>
+                        <div class="hwcard-author-d">
+                        <div class="hwcard-author-d-e1">
+                            <div class="hwcard-author-avatar-d">
+                                <a href="<?= get_author_posts_url($author_data['author']->ID); ?>">
+                                    <?= get_avatar($author_data['author']->ID, 40); ?>
                                 </a>
                             </div>
+                            <div class="hwcard-author-data-d">
+                                <a href="<?= get_author_posts_url($author_data['author']->ID); ?>">
+                                    <span class="hwcard-author-name-d"><?= esc_html($author_data['author']->display_name); ?></span>
+                                </a>
+                                <span class="hwcard-author-desc-d">
+                                    <?= esc_html(mb_strlen($author_data['author_description'], 'UTF-8') > 15 ? mb_substr($author_data['author_description'], 0, 15, 'UTF-8') . '...' : $author_data['author_description']); ?>
+                                </span>
+                            </div>
+                            </div>
+                            <div class="hwcard-author-data-item-d">
+                                <i class="fa-regular fa-pen-to-square" style="font-size: 12px;"></i>
+                                <span class="hwcard-author-data-value-d"><?= count_user_posts($author_data['author']->ID); ?></span>
+                            </div>
+                        </div>
                     <?php endforeach; ?>
-                    </div>
-                <?php endif; ?>
+                </div>
+            <?php elseif ($author_info['type'] == 'multiple'): ?>
+                <div class="hwcard-content">
+                    <?php foreach ($author_info['authors'] as $author): ?>
+                        <div class="hwcard-author-avatar">
+                            <a href="<?= get_author_posts_url($author->ID); ?>">
+                                <?= get_avatar($author->ID, 48); ?>
+                            </a>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            <?php endif; ?>
         </div>
         <div class="homepage-widget-card">
             <div class="homepage-widget-card-info">
-            <i class="fa-solid fa-circle-nodes"></i><?php esc_attr_e('Social Network', 'sakurairo'); ?>
+                <i class="fa-solid fa-circle-nodes"></i><?php esc_attr_e('Social Network', 'sakurairo'); ?>
             </div>
             <div class="hwcard-content">
-            <div class="hwcard-socialnet">
-            <?php $print_social_zone(); ?>
-            </div>
+                <div class="hwcard-socialnet">
+                    <?php print_social_zone($all_opt, $social_display_icon); ?>
+                </div>
             </div>
         </div>
     </div>
@@ -219,7 +221,6 @@ if ($author_count == 1) {
 <?php
 echo bgvideo(); //BGVideo 
 ?>
-<!-- 首页下拉箭头 -->
 <?php if (iro_opt('drop_down_arrow', 'true')): ?>
     <div class="headertop-down" onclick="headertop_down()"><span><svg t="1682342753354" class="homepage-downicon" viewBox="0 0 1843 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="21355" width="60px" height="60px"><path d="M1221.06136021 284.43250057a100.69380037 100.69380037 0 0 1 130.90169466 153.0543795l-352.4275638 302.08090944a100.69380037 100.69380037 0 0 1-130.90169467 0L516.20574044 437.48688007A100.69380037 100.69380037 0 0 1 647.10792676 284.43250057L934.08439763 530.52766665l286.97696258-246.09516608z" fill="<?= iro_opt('drop_down_arrow_color'); ?>" p-id="21356"></path></svg></span></div>
 <?php endif; ?>
